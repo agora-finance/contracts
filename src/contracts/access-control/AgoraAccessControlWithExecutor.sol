c323037e191e1ab5b80ef860c0f5c14617c87fa7
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2025-02-15 14:54:31 -0500

    feat: AgoraAccessControlWithExecutor.sol

---
+++ src/contracts/access-control/AgoraAccessControlWithExecutor.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.28;
+
+import { AgoraAccessControl } from "./AgoraAccessControl.sol";
+
+contract AgoraAccessControlWithExecutor is AgoraAccessControl {
+    constructor(address _initialAdminAddress) {
+        _initializeAgoraAccessControl({ _initialAdminAddress: _initialAdminAddress });
+    }
+
+    function execute(address _target, bytes memory _data) external payable {
+        _requireSenderIsRole({ _role: ACCESS_CONTROL_ADMIN_ROLE });
+        (bool success, ) = _target.call{ value: msg.value }(_data);
+        if (!success) {
+            revert ExecuteCallFailed();
+        }
+    }
+
+    error ExecuteCallFailed();
+}
